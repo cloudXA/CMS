d0defbd7164abbd453df64db9f466b712484c17f
cloudXA <zming794@gmail.com>
2020-01-11 09:25:15 +0800

    新闻资讯列表跳转到新闻详情

--- src/router.js
+++ src/router.js
@@ -9,8 +9,8 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
-// import NewsList from './components/news/NewsList.vue'
-// import NewsInfo from './components/news/NewsInfo.vue'
+import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
@@ -30,8 +30,8 @@ var router = new VueRouter({
     { path: '/shopcar', component: ShopcarContainer },
     { path: '/search', component: SearchContainer },
     // 主页面下的newlist和newsifon链接导航TODO:
-    // { path: '/home/newslist', component: NewsList },
-    // { path: '/home/newsinfo/:id', component: NewsInfo }
+    { path: '/home/newslist', component: NewsList },
+    { path: '/home/newsinfo/:id', component: NewsInfo }
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
